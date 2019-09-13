cf098ecece7949cb4e6ef6b5ae594a7d9add2282
asus <asus@10.89.168.77>
2019-09-13 09:39:28 +0200

    Initial project setup

---
+++ src/main/java/Library.java
@@ -0,0 +1,12 @@
+
+public class Library {
+    
+	public static void main( String[] args) {
+		System.out.println("When the fire is gone");
+	}
+    
+    public boolean someLibraryMethod() {
+    	return true;
+    }
+    
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,12 @@
+
+public class LibraryTest {
+    
+	public static void main( String[] args) {
+		System.out.println("When the fire is gone");
+	}
+    
+    public boolean someLibraryMethod() {
+    	return true;
+    }
+    
+}
